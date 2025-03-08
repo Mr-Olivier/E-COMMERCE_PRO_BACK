73aaf00b2e604d157e3a2c0575529f28fe159706
Olivier <iradukundaolivier2000@gmail.com>
2025-03-08 11:14:49 +0200

    build: update compiled TypeScript files

--- dist/routes/auth.routes.js
+++ dist/routes/auth.routes.js
@@ -38,6 +38,7 @@ const express_1 = require("express");
 const authController = __importStar(require("../controllers/auth.controller"));
 const validation_middleware_1 = require("../middlewares/validation.middleware");
 const auth_validation_1 = require("../validations/auth.validation");
+const auth_controller_1 = require("../controllers/auth.controller");
 const router = (0, express_1.Router)();
 // Register
 router.post("/register", (0, validation_middleware_1.validate)(auth_validation_1.registerValidation), authController.register);
@@ -47,4 +48,11 @@ router.post("/verify-otp", (0, validation_middleware_1.validate)(auth_validation
 router.post("/login", (0, validation_middleware_1.validate)(auth_validation_1.loginValidation), authController.login);
 // Resend OTP
 router.post("/resend-otp", (0, validation_middleware_1.validate)([auth_validation_1.verifyOtpValidation[0]]), authController.resendOtp);
+// Add these routes to your auth.routes.ts file
+// Forgot password - request reset
+router.post("/forgot-password", (0, validation_middleware_1.validate)(auth_validation_1.forgotPasswordValidation), auth_controller_1.forgotPassword);
+// Verify reset OTP
+router.post("/verify-reset-otp", (0, validation_middleware_1.validate)(auth_validation_1.verifyResetOtpValidation), auth_controller_1.verifyResetOtp);
+// Reset password with new password
+router.post("/reset-password", (0, validation_middleware_1.validate)(auth_validation_1.resetPasswordValidation), auth_controller_1.resetPassword);
 exports.default = router;
