1ac1cf174cdd6995f85cfd0c228ee708d0027fd8
Olivier <iradukundaolivier2000@gmail.com>
2025-03-10 23:38:19 +0200

    build: add compiled file upload utility

---
+++ dist/utils/upload.js
@@ -0,0 +1,47 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.productImageUpload = void 0;
+const multer_1 = __importDefault(require("multer"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+// Create uploads directory if it doesn't exist
+const uploadDir = path_1.default.join(process.cwd(), "uploads");
+if (!fs_1.default.existsSync(uploadDir)) {
+    fs_1.default.mkdirSync(uploadDir, { recursive: true });
+}
+// Configure storage
+const storage = multer_1.default.diskStorage({
+    destination: (req, file, cb) => {
+        const productImagesDir = path_1.default.join(uploadDir, "products");
+        if (!fs_1.default.existsSync(productImagesDir)) {
+            fs_1.default.mkdirSync(productImagesDir, { recursive: true });
+        }
+        cb(null, productImagesDir);
+    },
+    filename: (req, file, cb) => {
+        // Create unique filename with original extension
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const ext = path_1.default.extname(file.originalname);
+        cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+    },
+});
+// File filter to accept only images
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+        cb(null, true);
+    }
+    else {
+        cb(new Error("Only image files are allowed"));
+    }
+};
+// Create the multer upload instance
+exports.productImageUpload = (0, multer_1.default)({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB max file size
+    },
+});
